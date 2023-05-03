5f29b3a746ca307e7fb71a5c337d3c238bd0c5f1
peniamati <pena_matias@hotmail.com>
2023-05-03 15:47:18 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            hola q pasa asasa asfafagasgasfasf
+            Console.ReadLine();
+            papapa
+
+
+
+
+            
+        }
+    }
+}
